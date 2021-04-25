06013722b9b68c933f2daf85eaa9dcc4bd943ff9
kyoya <kyoyaprog@gmail.com>
2021-04-25 23:31:13 +0900

    swiftlint autocorrect

--- PatientMoney/ViewController.swift
+++ PatientMoney/ViewController.swift
@@ -5,18 +5,13 @@
 //  Created by 松山響也 on 2021/04/25.
 //
 
-import UIKit
-import FirebaseFirestore
 import Firebase
-
+import FirebaseFirestore
+import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
