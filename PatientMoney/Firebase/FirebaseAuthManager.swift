ebb5cfa844a72262db228f532f582bc8921c0ea0
kyoya <kyoyaprog@gmail.com>
2021-04-29 15:16:27 +0900

    FirebaseAuthManagerの実装

---
+++ PatientMoney/Firebase/FirebaseAuthManager.swift
@@ -0,0 +1,32 @@
+import FirebaseAuth
+import Foundation
+
+/// Firebase認証マネージャー
+class FirebaseAuthManeger {
+    /// 共有インスタンス
+    let shared = FirebaseAuthManeger()
+
+    /// サインインしているかどうか
+    var isSignIn: Bool = {
+        Auth.auth().currentUser != nil
+    }()
+
+    /// サインインする
+    func signIn(email: String, password: String, completion: @escaping (AuthDataResult?, Error?) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            completion(result, error)
+        }
+    }
+
+    /// サインアップする
+    func signUp(email: String, password: String, completion: @escaping (AuthDataResult?, Error?) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            completion(result, error)
+        }
+    }
+
+    /// サインアウトする
+    func signOut() throws {
+        try Auth.auth().signOut()
+    }
+}
