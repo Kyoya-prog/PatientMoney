fecff4665a8cebb2573c8d641a3ce82987eca2bc
kyoya <kyoyaprog@gmail.com>
2021-05-01 21:30:29 +0900

    インデントを調整

--- PatientMoney/Extension/NSAttributedStrinfExtension.swift
+++ PatientMoney/Extension/NSAttributedStrinfExtension.swift
@@ -1,14 +1,8 @@
 import UIKit.NSAttributedString
 
 extension NSAttributedString {
-    static func icon(
-        _ icon: FontAwesome.Icon,
-        size: CGFloat,
-        style: FontAwesome.Style = .solid)
-    -> NSAttributedString {
-        NSAttributedString(
-            string: icon.code,
-            attributes: [
+    static func icon(_ icon: FontAwesome.Icon,size: CGFloat,style: FontAwesome.Style = .solid)-> NSAttributedString {
+        NSAttributedString(string: icon.code,attributes: [
                 .font: FontAwesome.font(size: size, style: style)
             ]
         )
