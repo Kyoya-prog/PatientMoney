173625cc9d30b49e25285710720be3ee0a369299
kyoya <kyoyaprog@gmail.com>
2021-05-01 13:45:13 +0900

    モジュールのアセンブルメソッドを実装

--- PatientMoney/Auth/Router/AuthRouter.swift
+++ PatientMoney/Auth/Router/AuthRouter.swift
@@ -4,30 +4,49 @@ import UIKit
 class AuthRouter: AuthWireFrame {
     var viewController: UIViewController?
 
-    func presentSignUpView() {
-        let signUpView = SignUpViewController()
-        let presenter = AuthPresenter()
-        let router = AuthRouter()
-
-        router.viewController = signUpView
-        presenter.router = router
-        signUpView.presenter = presenter
-
-        signUpView.modalPresentationStyle = .fullScreen
-        viewController?.present(signUpView, animated: true, completion: nil)
-    }
-
-    func presentSignInView() {
+    static func assembleSignInModule() -> UIViewController {
         let signInView = SignInViewController()
         let presenter = AuthPresenter()
         let router = AuthRouter()
+        let interactor = AuthInteractor()
 
         router.viewController = signInView
         presenter.router = router
+        presenter.interactor = interactor
+        presenter.view = signInView
+        interactor.output = presenter
         signInView.presenter = presenter
 
-        signInView.modalPresentationStyle = .fullScreen
-        viewController?.present(signInView, animated: true, completion: nil)
+        return signInView
+    }
+
+    static func assembleSignUpModule() -> UIViewController {
+        let signUpView = SignUpViewController()
+        let presenter = AuthPresenter()
+        let router = AuthRouter()
+        let interactor = AuthInteractor()
+
+        router.viewController = signUpView
+        presenter.router = router
+        presenter.interactor = interactor
+        presenter.view = signUpView
+        interactor.output = presenter
+        signUpView.presenter = presenter
+
+        return signUpView
+    }
+    
+    // MARK:AuthWireFrame 
+    func presentSignUpView() {
+        let view = AuthRouter.assembleSignUpModule()
+        view.modalPresentationStyle = .fullScreen
+        viewController?.present(view, animated: true, completion: nil)
+    }
+
+    func presentSignInView() {
+        let view = AuthRouter.assembleSignInModule()
+        view.modalPresentationStyle = .fullScreen
+        viewController?.present(view, animated: true, completion: nil)
     }
 
     func presentHomeView() {
