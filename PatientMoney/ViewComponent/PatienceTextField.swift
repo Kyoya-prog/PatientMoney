c4df472f2a7c7e113134b75b7e246cc55152f86b
kyoya <kyoyaprog@gmail.com>
2021-05-02 14:45:31 +0900

    PatienceTextFieldにpaddingを設定できるようにした

--- PatientMoney/ViewComponent/PatienceTextField.swift
+++ PatientMoney/ViewComponent/PatienceTextField.swift
@@ -2,17 +2,20 @@ import UIKit
 
 /// padding付きのテキストフィールド（アプリ内では原則このtextfieldを使用する）
 class PatienceTextField: UITextField {
-    private let padding: CGFloat = 8
+    /// テキストの表示インセット
+    public var textInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10) {
+        didSet { layoutIfNeeded() }
+    }
 
     override func textRect(forBounds bounds: CGRect) -> CGRect {
-        bounds.inset(by: UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding))
+        bounds.inset(by: textInsets)
     }
 
     override func editingRect(forBounds bounds: CGRect) -> CGRect {
-        bounds.inset(by: UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding))
+        bounds.inset(by: textInsets)
     }
 
     override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
-        bounds.inset(by: UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding))
+        bounds.inset(by: textInsets)
     }
 }
