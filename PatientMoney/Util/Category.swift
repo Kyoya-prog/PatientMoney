268065cde08226661c4a03bb97cc35482e5109a3
kyoya <kyoyaprog@gmail.com>
2021-05-06 15:12:23 +0900

    アイコンタイトルの文字リソース専用のものを用意する

--- PatientMoney/Util/Category.swift
+++ PatientMoney/Util/Category.swift
@@ -2,8 +2,8 @@ import UIKit
 
 class Category {
     static let categories:[(icon: FontAwesome.Icon, title: String, color: UIColor)] = [
-        (icon:.pizzaSlice, title:L10n.CategoriesView.IconTitle.pizzaSlice, UIColor(hex: "FFA500")),
-        (icon:.bus, title:L10n.CategoriesView.IconTitle.bus, UIColor(hex: "00BFFF")),
-        (icon:.paintBrush, title:L10n.CategoriesView.IconTitle.paintBrush, UIColor(hex: "00FF00"))
+        (icon:.pizzaSlice, title:L10n.CategoryIcon.Title.pizzaSlice, UIColor(hex: "FFA500")),
+        (icon:.bus, title:L10n.CategoryIcon.Title.bus, UIColor(hex: "00BFFF")),
+        (icon:.paintBrush, title:L10n.CategoryIcon.Title.paintBrush, UIColor(hex: "00FF00"))
     ]
 }
