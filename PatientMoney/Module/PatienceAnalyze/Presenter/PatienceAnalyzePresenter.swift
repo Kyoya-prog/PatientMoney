d932fe1d597a8302ccbb27cca7ee0b8e96fc3dc9
kyoya <kyoyaprog@gmail.com>
2021-06-03 03:18:33 +0900

    Presenterの生成

---
+++ PatientMoney/Module/PatienceAnalyze/Presenter/PatienceAnalyzePresenter.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+class PatienceAnalyzePresenter:PatienceAnalyzePresentation,PatienceAnalyzeOutput{
+    
+    // MARK:PatienceAnalyzePresentation
+    var usecase: PatienceAnalyzeUsecase!
+    
+    var view: PatienceAnalyzeView?
+    
+    func didSelectMonth(year: Int, month: Int) {
+        usecase.fetchDataFromMonth(year: year, month: month)
+    }
+    
+    // MARK:PatienceAnalyzeOutput
+    func outputFetchRecords(records: [PatienceEntity]) {
+        view?.updateRecords(records: records)
+        var sumMoney = 0
+        records.forEach{
+            sumMoney += $0.money
+        }
+        view?.updateSumMoney(sumMoney: sumMoney)
+    }
+    
+    func outputError() {
+        view?.showError(message: "データの取得に失敗しました")
+    }
+    
+}
