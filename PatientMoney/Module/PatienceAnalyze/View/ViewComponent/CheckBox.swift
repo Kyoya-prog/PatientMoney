f0e313ed728e1ab57a678f7c63b0e830d7585c32
kyoya <kyoyaprog@gmail.com>
2021-06-19 16:33:14 +0900

    iconLabelがsuperViewより大きくなってしまい、タップに反応しなかったので修正（superViewからはみ出た部分はuseractionを受け付けないらしい）

--- PatientMoney/Module/PatienceAnalyze/View/ViewComponent/CheckBox.swift
+++ PatientMoney/Module/PatienceAnalyze/View/ViewComponent/CheckBox.swift
@@ -3,17 +3,21 @@ import UIKit
 
 /// CheckBox
 class CheckBox: UIControl {
+    /// チェックされているかどうか
     var isOn = false {
         didSet {
             updateDisplay()
         }
     }
-    
+
+    /// チェックボックスが選択された時のアクション
+    var checkBoxSelectedAction:((_ isOn: Bool) -> Void)?
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         construct()
     }
-    
+
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         construct()
@@ -27,21 +31,25 @@ class CheckBox: UIControl {
         iconLabel.clipsToBounds = true
         addSubview(iconLabel)
         NSLayoutConstraint.activate([
+            heightAnchor.constraint(equalToConstant: 30),
+            widthAnchor.constraint(equalToConstant: 30),
             iconLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
             iconLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
             iconLabel.widthAnchor.constraint(equalToConstant: 30),
             iconLabel.heightAnchor.constraint(equalToConstant: 30)
         ])
-        
+
         updateDisplay()
     }
-    
+
     @objc private func touchAction(_ sender: UIControl) {
         isOn.toggle()
+        backgroundColor = .red
+        checkBoxSelectedAction?(isOn)
     }
-    
+
     private let iconLabel = UILabel()
-    
+
     private func updateDisplay() {
         if !isOn {
             iconLabel.layer.borderWidth = 1
@@ -50,17 +58,17 @@ class CheckBox: UIControl {
         case (false, false):
             iconLabel.textColor = .black
             iconLabel.backgroundColor = .white
-            
+
         case (true, false):
             iconLabel.textColor = .white
             iconLabel.backgroundColor = .gray
-            
+
         case (_, true):
             iconLabel.textColor = .white
             iconLabel.backgroundColor = UIColor(hex: "cccccc")
         }
     }
-    
+
     override var isHighlighted: Bool {
         didSet {
             updateDisplay()
@@ -73,15 +81,15 @@ import  SwiftUI
 
 struct CheckBoxWrapper: UIViewRepresentable {
     var isOn: Bool
-    
+
     var isHighlighted: Bool
-    
+
     typealias UIViewType = CheckBox
-    
+
     func makeUIView(context: Context) -> CheckBox {
         CheckBox()
     }
-    
+
     func updateUIView(_ uiView: CheckBox, context: Context) {
         uiView.isOn = isOn
         uiView.isHighlighted = isHighlighted
@@ -96,7 +104,6 @@ struct CheckBoxPreviews: PreviewProvider {
         CheckBoxWrapper(isOn: true, isHighlighted: false
         ).previewDisplayName("isOn and not highlighted")
         .previewLayout(.fixed(width: 50, height: 50))
-        
         CheckBoxWrapper(isOn: false, isHighlighted: false
         ).previewDisplayName("not selected and not highlighted")
         .previewLayout(.fixed(width: 50, height: 50))
