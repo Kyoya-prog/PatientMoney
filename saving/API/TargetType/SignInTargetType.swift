d78874d01121cd86a9828e5719ac2bef7783b10d
kyoya <kyoyaprog@gmail.com>
2021-08-13 21:42:31 +0900

    SignInTargetTypeを実装

---
+++ saving/API/TargetType/SignInTargetType.swift
@@ -0,0 +1,21 @@
+import Foundation
+import Moya
+
+struct SignInTargetType: ApiTargetType {
+    typealias Response = OAuthTokenEntity
+
+    let email: String
+
+    let password: String
+    
+    var parameters: [String: String] { ["email": email, "password": password] }
+
+    var path: String { "/session" }
+
+    var method: Moya.Method { .post }
+
+    var sampleData: Data
+
+    var task: Task { .requestParameters(parameters: parameters, encoding: URLEncoding.default) }
+}
+
