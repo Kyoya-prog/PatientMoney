cb3ca3eaaabd1b2d72e960804fae7dede490320c
kyoya <kyoyaprog@gmail.com>
2021-08-16 15:47:49 +0900

    エラーメッセージの製造を一つのメソッドに置く

--- saving/Util/ErrorMessageBuilder.swift
+++ saving/Util/ErrorMessageBuilder.swift
@@ -4,20 +4,6 @@ import Moya
 
 class ErrorMessageBuilder {
     static func buildErrorMessage(error: Error, message: String) -> String {
-        var errorMessage = message
-        // NSError型を得るためMoyaErrorの中のNSUnderlyingErrorを抽出し、NSErrorにキャスト
-        if let moyaError = error as? MoyaError, let error = ((moyaError.errorUserInfo["NSUnderlyingError"]as? Alamofire.AFError)?.underlyingError as NSError?) {
-            switch error.code {
-            case -1009: // NSURLErrorNotConnectedToInternet　（ネットワーク接続エラーの場合は、その趣旨をエラーメッセージの後につける）
-                errorMessage = message + "(インターネット接続がありません)"
-            default:
-                errorMessage = message
-            }
-        }
-        return errorMessage
-    }
-
-    static func buildAuthErrorMessage(error: Error) -> String {
         guard let error = error as? MoyaResponseError else { return "処理に失敗しました" }
         switch error {
         case let .badRequestError(code):
@@ -31,22 +17,22 @@ class ErrorMessageBuilder {
             case 2003:
                 return "メールアドレスまたはパスワードが間違っています"
             default:
-                return "処理に失敗しました"
+                return message
             }
 
         case let .moyaError(error):
             if let error = ((error.errorUserInfo["NSUnderlyingError"]as? Alamofire.AFError)?.underlyingError as NSError?) {
                 switch error.code {
                 case -1009:
-                    return "インターネット接続がありません"
+                    return message + "(インターネット接続がありません)"
                 default:
-                    return "処理に失敗しました"
+                    return message
                 }
             }
 
         case .unknownError:
-            return "処理に失敗しました"
+            return message
         }
-        return "処理に失敗しました"
+        return message
     }
-}
+    }
