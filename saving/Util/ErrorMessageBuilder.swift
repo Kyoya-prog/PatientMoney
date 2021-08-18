a1c1e43541693c69f90471c34d4bfbfc3d007a85
kyoya <kyoyaprog@gmail.com>
2021-08-18 15:46:47 +0900

    文字列の対応もれを修正

--- saving/Util/ErrorMessageBuilder.swift
+++ saving/Util/ErrorMessageBuilder.swift
@@ -4,18 +4,18 @@ import Moya
 
 class ErrorMessageBuilder {
     static func buildErrorMessage(error: Error, message: String) -> String {
-        guard let error = error as? MoyaResponseError else { return "処理に失敗しました" }
+        guard let error = error as? MoyaResponseError else { return message }
         switch error {
         case let .badRequestError(code):
             switch code {
             case 2000:
-                return "既に使われているメールアドレスです"
+                return L10n.ErrorMessageBuilder.takenMailAddress
             case 2001:
-                return "パスワードが短すぎます。8文字以上入力してください"
+                return L10n.ErrorMessageBuilder.passwordTooShort
             case 2002:
-                return "正しいメールアドレスを入力してください"
+                return L10n.ErrorMessageBuilder.takenMailAddress
             case 2003:
-                return "メールアドレスまたはパスワードが間違っています"
+                return L10n.ErrorMessageBuilder.userNotFound
             default:
                 return message
             }
@@ -24,7 +24,7 @@ class ErrorMessageBuilder {
             if let error = ((error.errorUserInfo["NSUnderlyingError"]as? Alamofire.AFError)?.underlyingError as NSError?) {
                 switch error.code {
                 case -1009:
-                    return message + "(インターネット接続がありません)"
+                    return message + L10n.ErrorMessageBuilder.SubMessage.cannotConnectNetwork
                 default:
                     return message
                 }
