072bb282358df89521e66a0c5ffc60d2fce6900a
kyoya <kyoyaprog@gmail.com>
2021-08-20 15:08:50 +0900

    日付ごとのリクエストのTargetTypeを作成

---
+++ saving/API/TargetType/PatiencePerDateTargetType.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+import Foundation
+import Moya
+
+struct PatiencePerDateTargetType: ApiTargetType {
+    typealias Response = [PatienceEntity]
+
+    let date:Date
+
+    var path: String { "/patience/per_day" }
+
+    var method: Moya.Method { .get }
+
+    var sampleData: Data { Data() }
+
+    var task: Task { .requestParameters(parameters:
+                                            ["date":date], encoding: URLEncoding.queryString) }
+}
