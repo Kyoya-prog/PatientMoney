210dcf06355e1bbd9bfc2f151a4d50b03caabf17
kyoya <kyoyaprog@gmail.com>
2021-08-21 20:23:31 +0900

    日付の操作関係の処理をDate型のextensionに実装

---
+++ saving/Extension/DateExtension.swift
@@ -0,0 +1,74 @@
+import Foundation
+
+extension Date {
+    init(year: Int? = nil, month: Int? = nil, day: Int? = nil, hour: Int? = nil, minute: Int? = nil, second: Int? = nil) {
+        self.init(
+            timeIntervalSince1970: Date().fixed(
+                year: year,
+                month: month,
+                day: day,
+                hour: hour,
+                minute: minute,
+                second: second
+            ).timeIntervalSince1970
+        )
+    }
+
+    func fixed(year: Int? = nil, month: Int? = nil, day: Int? = nil, hour: Int? = nil, minute: Int? = nil, second: Int? = nil) -> Date {
+        let calendar = self.calendar
+
+        var comp = DateComponents()
+        comp.year   = year ?? calendar.component(.year, from: self)
+        comp.month  = month ?? calendar.component(.month, from: self)
+        comp.day    = day ?? calendar.component(.day, from: self)
+        comp.hour   = hour ?? calendar.component(.hour, from: self)
+        comp.minute = minute ?? calendar.component(.minute, from: self)
+        comp.second = second ?? calendar.component(.second, from: self)
+        //swiftlint:disable:next force_unwrapping
+        return calendar.date(from: comp)!
+    }
+
+    var year: Int {
+        calendar.component(.year, from: self)
+    }
+
+    var month: Int {
+        calendar.component(.month, from: self)
+    }
+
+    var day: Int {
+        calendar.component(.day, from: self)
+    }
+
+    var hour: Int {
+        calendar.component(.hour, from: self)
+    }
+
+    var minute: Int {
+        calendar.component(.minute, from: self)
+    }
+
+    var second: Int {
+        calendar.component(.second, from: self)
+    }
+
+    var calendar: Calendar {
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.timeZone = .japan
+        calendar.locale   = .japan
+        return calendar
+    }
+
+    var zeroclock: Date {
+        // APIリクエスト時にDate型そのままで送るとUTC時間になり、９時間前の時間になるので、９時間追加する
+        return fixed(hour: 9, minute: 0, second: 0)
+    }
+
+    var beginMonth: Date {
+        fixed(day: 1, hour: 9, minute: 0, second: 0)
+    }
+
+    var endMonth: Date {
+        fixed(month: month + 1, day: 0, hour: 9, minute: 0, second: 0)
+    }
+}
