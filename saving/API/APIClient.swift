5cb74be05153cd23278717390469d79008cc7f43
kyoya <kyoyaprog@gmail.com>
2021-08-26 21:22:48 +0900

    APIに日付を送信するときに、今まで行っていたUTC時間の配慮の部分を廃止する

--- saving/API/APIClient.swift
+++ saving/API/APIClient.swift
@@ -14,10 +14,12 @@ class ApiClient: ApiClientInterface {
         let provider = MoyaProvider<T>()
         provider.request(request, callbackQueue: callbackQueue) { result in
             let dateFormatter: DateFormatter = {
-                let df = DateFormatter()
-                df.dateFormat = "yyyy-MM-dd"
-                df.timeZone = TimeZone(secondsFromGMT: 0)
-                return df
+                let formatter = DateFormatter()
+                formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+                formatter.calendar = Calendar(identifier: .iso8601)
+                formatter.timeZone = TimeZone(secondsFromGMT: 0)
+                formatter.locale = Locale(identifier: "en_US_POSIX")
+                return formatter
             }()
             let decoder = JSONDecoder()
             decoder.dateDecodingStrategy = .formatted(dateFormatter)
