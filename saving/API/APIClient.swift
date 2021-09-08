57c430c494434e8d677832ce5336bf9bc83a9922
kyoya <kyoyaprog@gmail.com>
2021-09-08 20:25:33 +0900

    APIレスポンスのDateのformat形式を変更

--- saving/API/APIClient.swift
+++ saving/API/APIClient.swift
@@ -15,7 +15,7 @@ class ApiClient: ApiClientInterface {
         provider.request(request, callbackQueue: callbackQueue) { result in
             let dateFormatter: DateFormatter = {
                 let formatter = DateFormatter()
-                formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+                formatter.dateFormat = "yyyy-MM-dd"
                 formatter.calendar = Calendar(identifier: .iso8601)
                 formatter.timeZone = TimeZone(secondsFromGMT: 0)
                 formatter.locale = Locale(identifier: "en_US_POSIX")
