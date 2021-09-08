e315d30256cd99a1c5cbf8e8401d33f6f2d70378
kyoya <kyoyaprog@gmail.com>
2021-09-08 23:52:12 +0900

    ErrorMessageBuilderの処理を見直し

--- saving/Util/ErrorMessageBuilder.swift
+++ saving/Util/ErrorMessageBuilder.swift
@@ -8,48 +8,40 @@ class ErrorMessageBuilder {
             let apiError = checkMoyaError(error: error)
             return message + "(\(apiError.message))"
         }
-
+        
         return message
     }
     
-    private static func checkMoyaError(error: MoyaError)->APIError{
-        switch error {
-        case .objectMapping(_, _):
-            return .badRequestError
-
-        case .underlying(_, _):
-            return checkUnderLyingMoyaError(error: error)
-
-        case .statusCode(_):
-            return .serverError
-
-        default:
-            return .unknownError(error)
-        }
-    }
-
-    private static func checkUnderLyingMoyaError(error: MoyaError) -> APIError {
+    private static func checkMoyaError(error: MoyaError) -> APIError {
         if let error = ((error.errorUserInfo["NSUnderlyingError"]as? Alamofire.AFError)?.underlyingError as NSError?) {
             switch error.code {
             case -1004:
-                return .serverError
-
-            case -1009:
                 return .connectionError
-
+                
+            case -1009:
+                return .networkError
+                
             default:
                 return .unknownError(error)
             }
         } else {
-            return .unknownError(error)
+            switch error {
+            case .objectMapping(_, _):
+                return .badRequestError
+                
+            case .statusCode(_):
+                return .serverError
+                
+            default:
+                return .unknownError(error)
+            }
         }
     }
 
     public enum APIError: Error {
         case connectionError
         case serverError
-        case parseError
-        case unknownError(_ error:Error)
+        case unknownError(_ error: Error)
         case networkError
         case badRequestError
 
@@ -59,8 +51,6 @@ class ErrorMessageBuilder {
                 return "サーバーと通信できません。kyoyaprog@gmail.comまでお問い合わせください。"
             case .serverError:
                 return "サーバーでエラーが発生しています。kyoyaprog@gmail.comまでお問い合わせください"
-            case .parseError:
-                return "パースに失敗しています"
             case .unknownError:
                 return "不明なエラーです"
             case .networkError:
